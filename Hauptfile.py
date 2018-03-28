5edea962ce3b549b6192e8e38248e51c525ed456
Lena <sturmiechen@googlemail.com>
2018-03-28 19:24:24 +0200

    pygame imported, sys imported, erased example code

--- Hauptfile.py
+++ Hauptfile.py
@@ -1,32 +1,6 @@
-from math import pow, sqrt
+import pygame
+import sys
+#sys brauchen wir um später wieder rauszukommen...
 
 surface = pygame.image.load('beispiel.png').convert()
 
-class clsRechteck(object):
-    def __init__(self, a, b):
-        self.kanteA = a
-        self.kanteB = b
-
-    def setKanteA(self, a):
-        self.kanteA = a
-
-    def setKanteB(self, b):
-        self.kanteB = b
-
-    def getKanteA(self):
-        return self.kanteA
-
-    def getKanteB(self):
-        return self.kanteB
-
-    def getKanteC(self):
-        cQuadrat = pow(self.kanteA, 2) + (self.kanteB ** 2)
-        kanteC = sqrt(cQuadrat)
-
-        return kanteC
-
-    def getFlaeche(self):
-        if (self.kanteA > 0) and (self.kanteB > 0):
-            return (self.kanteA * self.kanteB) / 2
-        else:
-            return 0
