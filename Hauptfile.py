380e8aa81b14c58445ab58b224c3c79e6336c67e
Lena <sturmiechen@googlemail.com>
2018-03-28 19:56:02 +0200

    added content to Weltkarte, added colouration, added import self-written module from weltkarte to Haupfile, new surface with worldmap content

--- Hauptfile.py
+++ Hauptfile.py
@@ -1,9 +1,13 @@
 import pygame, sys
+import Weltkarte
 
 #colour resource: https://www.rapidtables.com/web/color/RGB_Color.html
 
 
 pygame.init()
+surfacemap=pygame.display.set_mode((Weltkarte.MAPWIDTH*Weltkarte.TILESIZE, Weltkarte.MAPHEIGHT*Weltkarte.TILESIZE))
+
+
 surface = pygame.image.load('beispiel.png').convert()
 
 pygame.display.set_caption("Beispiel")
