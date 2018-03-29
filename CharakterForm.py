4bc02eb7247ba3b499498a46de4d59ada29d608f
Andre <andre.rohrberg@gmail.com>
2018-03-29 15:07:11 +0200

    inventory added, exception handling completed

--- CharakterForm.py
+++ CharakterForm.py
@@ -3,7 +3,7 @@ import Weltkarte
 
 
 CHARACTER=pygame.image.load('characterbear.png')
-CHARACTER=pygame.transform.scale(CHARACTER,(Weltkarte.TILESIZE,Weltkarte.TILESIZE))
+CHARACTER=pygame.transform.scale(CHARACTER,(Weltkarte.TILESIZE,Weltkarte.TILESIZE)) 
 POSITION=[0,0]
 
 
