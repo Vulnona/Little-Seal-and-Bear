6b027cc097a0832d397e7bf7bc72765d7b985dd0
Andre <andre.rohrberg@gmail.com>
2018-03-30 15:57:49 +0200

    Merge branch 'master' of https://github.com/Vulnona/Pythonprojekt
    # Conflicts:
    #	.idea/workspace.xml

--- LevelupForm.py
+++ LevelupForm.py
@@ -3,25 +3,31 @@ import Hauptfile
 # pygame.draw.polygon
 # for Andre
 
+
 class Star(object):
     def __init__(self):
-        self.Zackenzahl=zackzahl
-        self.Farbe=farbe
-        self.Groesse=groesse
+        self.Zackenzahl = zackzahl
+        self.Farbe = farbe
+        self.Groesse = groesse
+
     def getZackenanzahl(self):
         pass
+
     def setZackenanzahl(self):
         pass
+
     def setKoordinaten(self):
         pass
+
     def getKoordinaten(self):
         pass
+
     penta = [(55, 20), (75, 80), (20, 40), (80, 40), (35, 80)]
 
     GOLD = (255, 215, 0)
     DARKRED = (139, 0, 0)
 
-#pygame.display.set_caption("Screentitle")
+# pygame.display.set_caption("Screentitle")
 
     # pointlists for different stars
     # inverse
@@ -33,4 +39,4 @@ class Star(object):
 # pygame.draw.lines(screen, darkred, 1, pointlist, 3)
 
 
-#pygame.display.update()
+# pygame.display.update()
