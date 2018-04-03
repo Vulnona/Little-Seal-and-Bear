f4763de4bf5ba37f4a1b670a64e9f5fd0031faec
Lena <sturmiechen@googlemail.com>
2018-04-03 11:56:39 +0200

    herstellen buttons placed and labeled

--- Interaktion.py
+++ Interaktion.py
@@ -21,8 +21,12 @@ class Menu(object):
                 else:
                     label = INVENTARFONT.render("Craftables: ", 0, Farben.clsFarben.WHITE)
                     exitbutton = pygame.Rect(480, 420, 80, 20)
+                    craftbuttonx=100
+                    craftbuttony=200
                     pygame.draw.rect(self.screen, Farben.clsFarben.DARKRED, exitbutton)
+                    # pygame.draw.rect(self.screen, Farben.clsFarben.GOLD, craftbuttons[item])
                     self.screen.blit(label, (100, 125))
+
                     placePosition=150
                     for item in Weltkarte.craftables:
                         self.screen.blit(Weltkarte.craftsnippets[item],(120, placePosition))
@@ -30,6 +34,11 @@ class Menu(object):
                         textObjekt = INVENTARFONT.render(str(Weltkarte.inventorycrafts.get(item)), True, Farben.clsFarben.WHITE,Farben.clsFarben.BLACK)
                         self.screen.blit(textObjekt, (100, placePosition-40))
                         placePosition += 40
+                        craftbutton = pygame.Rect(craftbuttonx, craftbuttony, 60, 15)
+                        pygame.draw.rect(self.screen, Farben.clsFarben.BROWN, craftbutton)
+                        craftlabel = INVENTARFONT.render("Herstellen", 0, Farben.clsFarben.GOLD)
+                        self.screen.blit(craftlabel, (craftbuttonx + 5, craftbuttony - 1))
+                        craftbuttony+=100
                     if event.type == MOUSEBUTTONDOWN:
                         mousepos = event.pos
                         if exitbutton.collidepoint(mousepos):
@@ -44,6 +53,10 @@ class Menu(object):
         BG = pygame.Rect(45, 75, 500, 500)
         exitbutton = pygame.Rect(480, 420, buttonwidth, buttonheigth)
         feedbutton = pygame.Rect(280, 400, buttonwidth, buttonheigth)
+        #craftbutton1= pygame.Rect(90,200, buttonwidth, buttonheigth)
+        #craftbutton2 = pygame.Rect(90, 240, buttonwidth, buttonheigth)
+        #craftbutton3= pygame.Rect(90, 280, buttonwidth, buttonheigth)
+        #craftbuttons={craftbutton1,craftbutton2,craftbutton3}
         label = INVENTARFONT.render("Zurück", 1, Farben.clsFarben.WHITE)
         feedlabel= INVENTARFONT.render("Füttern", 1, Farben.clsFarben.WHITE)
         proceed = True
@@ -67,7 +80,7 @@ class Menu(object):
                         placePosition += 30
                         textObjekt = INVENTARFONT.render(str(Weltkarte.inventory[item]), True, (255,255,255),(0,0,0))
                         self.screen.blit(textObjekt, (placePosition, Weltkarte.MAPHEIGHT * Weltkarte.TILESIZE + 20))
-                        placePosition += 50
+                        placePosition += 35
                     if charakter.animaltype == "baer":
                         if charakter.level<4:
                             image = pygame.image.load('babybear.png').convert()
